fb6e1a5c921c85186bbe28864973ca6d415540e8
yang chang qiu <570274003@qq.com>
2014-07-17 17:31:28 +0800

    modify wechat basic set info

--- Wechat/Admin/Controller/wechatSetController.class.php
+++ Wechat/Admin/Controller/wechatSetController.class.php
@@ -1,10 +1,16 @@
+<?php
 namespace Admin\Controller;
 use Think\Controller;
-class wechatSetController extends Controller {
+class WechatSetController extends Controller {
 
 
    public function interfaceSet(){
 
+     $data=I("get.dataformat");
+       echo json_encode($data);
+     //  $fh = fopen("./Public/resource/config.json", "w");
+      // fwrite($fh, $data);
+     //  fclose($fh);
 
     }
 
